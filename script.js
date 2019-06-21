2f8ab160ff8d26c57a3901869b46ef17f370e58a
Zachary Liou <zachliou@gmail.com>
2019-06-20 21:58:32 -0700

    Update colors, data, name

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
+var monthlyPrices = [-2000, -500, 1200, 3000, 3050, 2800, 4500];
+
 var ctx = document.getElementById('chart1').getContext('2d');
 var chart = new Chart(ctx, {
     // The type of chart we want to create
-    type: 'line',
+    type: 'bar',
 
     // The data for our dataset
     data: {
         labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
         datasets: [{
-            label: 'My First dataset',
-            backgroundColor: 'rgb(255, 99, 132)',
-            borderColor: 'rgb(255, 99, 132)',
-            data: [0, 10, 5, 2, 20, 30, 45]
+            label: 'Total revenue',
+            backgroundColor: 'rgb(125, 176, 190)',
+            borderColor: 'rgb(0, 0, 0)',
+            data: monthlyPrices
         }]
     },
 
